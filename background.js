f00bed840c0b011ffd7dc548321f77b5d8724117
cryterpe <60299726+cryterpe@users.noreply.github.com>
2020-02-02 11:54:43 -0800

    Add files via upload
    removed unnecessary functions, added 'chrome'

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {schemes: ['http', 'https']},
+            pageUrl: {schemes: ['http', 'https','chrome']},
           })
-          ],
+        ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
       });
