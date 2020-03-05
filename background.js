4ee4b4e0ab5e3c07d95653acc07c58e5a9515bca
Kim Bui <kimie2b@Kims-MacBook-Pro-2.local>
2020-03-04 19:54:21 -0800

    button works

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {schemes: ['http', 'https','chrome']},
+            pageUrl: {schemes: ['http', 'https','chrome','chrome-extension']},
           })
         ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
--- options.js
+++ options.js
@@ -1,19 +1,26 @@
 
-var newName = document.getElementById('className').value;
-var day1 = document.getElementById('C1').value;
-var day2 = document.getElementById('C2').value;
-var day3 = document.getElementById('C3').value;
-var day4 = document.getElementById('C4').value;
-var day5 = document.getElementById('C5').value;
-var start = document.getElementById('classStartTime').value;
-var end = document.getElementById('classStartTime').value;
 
-function addClass(){
+let newName = document.getElementById('className');
+let day1 = document.getElementById('C1');
+let day2 = document.getElementById('C2');
+let day3 = document.getElementById('C3');
+let day4 = document.getElementById('C4');
+let day5 = document.getElementById('C5'); 
+let start = document.getElementById('classStartTime');
+let end = document.getElementById('classEndTime');
 
-  var today = new Date();
-  
+function addClass()
+{
+  var today = new Date(); 
   var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
   document.write(date);
+  alert("hello");
 }
 
-addClass();
+document.getElementById("submit").addEventListener("click", function(){
+  document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + 
+      newName.value + ": " + start.value + "-" + end.value + "<br>";
+});
+
+
+
